b7fb64b718b1d1afebc1e7e26cc75595dafc0b7d
darktend <softwaredarktend@gmail.com>
2023-09-06 11:31:45 +0300

    fix padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,10 @@
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { useCallback, useEffect, useState } from "react";
+import {useWindowSize} from "../hooks/useWindowSize.js";
 
 const Hero = () => {
-    const [windowWidth, setWindowWidth] = useState(0);
-    const [windowHeight, setWindowHeight] = useState(0);
-
-    const handleResize = useCallback(() => {
-        setWindowWidth(window.innerWidth);
-        setWindowHeight(window.innerHeight);
-    }, []);
-
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.addEventListener("resize", handleResize);
-            handleResize();
-            return () => {
-                window.removeEventListener("resize", handleResize);
-            };
-        }
-    }, [handleResize]);
-
-    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
-    const hideComputersCanvas = (isPortrait && windowWidth <= 640) || windowHeight <= 420;
+    const {width,height,isPortrait}=useWindowSize();
+    const hideComputersCanvas=(isPortrait && width <=640) || height <= 420
     return (
         <section className={`relative w-full mx-auto ${hideComputersCanvas ? 'h-96' : 'h-screen'}`}>
             <div
@@ -35,7 +17,7 @@ const Hero = () => {
 
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
-                        Hi, I'm <span className='text-[#915EFF]'>Vlad Godun</span>
+                        I'm <span className='text-[#915EFF]'>Vlad Godun</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                         I develop user interfaces<br className='sm:block hidden' />
