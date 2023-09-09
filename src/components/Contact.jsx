d8149d86a12658978e2c5bc5af87578fad0c889d
darktend <Fredifox1935>
2023-09-10 02:42:41 +0300

    create alert

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import { styles } from "../styles";
@@ -25,15 +25,25 @@ const Contact = () => {
     name: "",
     message: "",
   });
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
     setErrors({ ...errors, [name]: "" });
+    setAlertMessage("");
+    setAlertType("");
+  };
+
+  const clearMessages = () => {
+    setAlertMessage("");
+    setAlertType("");
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    clearMessages();
 
     const nameError = validateInput(form.name);
     const emailError = validateEmail(form.email);
@@ -58,11 +68,30 @@ const Contact = () => {
         );
         setLoading(false);
         setSubmitSuccess(true);
+        setAlertMessage(
+          "Thank you. I will get back to you as soon as possible."
+        );
+        setAlertType("success");
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
       } catch (error) {
         setLoading(false);
-        alert("Something went wrong");
+        setAlertMessage("Something went wrong");
+        setAlertType("error");
+      }
+    } else {
+      if (!(form.name || form.email || form.message)) {
+        setAlertMessage("Please fill in all fields.");
+      } else {
+        setAlertMessage(
+          "Thank you. I will get back to you as soon as possible."
+        );
       }
     }
+    setTimeout(clearMessages, 3000);
   };
 
   const renderInput = (name, label) => (
@@ -97,25 +126,31 @@ const Contact = () => {
 
   useEffect(() => {
     if (submitSuccess) {
-      alert("Thank you. I will get back to you as soon as possible.");
-      setForm({
-        name: "",
-        email: "",
-        message: "",
-      });
-      setSubmitSuccess(false);
+      setTimeout(() => {
+        setAlertMessage("");
+        setAlertType("");
+      }, 3000);
     }
   }, [submitSuccess]);
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      {alertMessage && (
+        <div
+          className={`fixed top-[10%] left-1/2 transform -translate-x-1/2 ${
+            alertType === "success" ? "bg-green-500" : "bg-red-500"
+          } text-white p-3 rounded-lg z-10 w-11/12 sm:w-10/12 text-center opacity-100 transition-opacity ease-in-out duration-300`}
+        >
+          {alertMessage}
+        </div>
+      )}
+
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
-
         <form
           ref={formRef}
           onSubmit={handleSubmit}
@@ -124,7 +159,6 @@ const Contact = () => {
           {renderInput("name", "Your Name")}
           {renderInput("email", "Your Email")}
           {renderTextarea("message", "Your Message")}
-
           <button
             type="submit"
             className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
